359072c82def8dabdec1fe2f89b4ffa37add9d09
hadley holquist <hh239017@ohio.edu>
2019-09-05 16:36:48 -0400

    changes to git-lab-program.cc

---
+++ git-lab-program.cc
@@ -0,0 +1,18 @@
+/*
+ *        File: git-lab-program.cc
+ *      Author: Hadley Holquist
+ *        Date: 09/05/2019
+ * Description: Hello Git
+ */
+
+#include <iostream>
+#include <iomanip>
+#include <cstdlib>
+using namespace std;
+
+
+int main(int argc, char const *argv[]) {
+
+    cout << "Hello Git!!" << endl;
+    return 0;
+}// main
